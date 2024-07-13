2d54da197602aaa91169abc1f9e329206ca0462c
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 16:27:39 -0500

    Create Internship.java

---
+++ Internship.java
@@ -0,0 +1,48 @@
+public class Internship {
+    private String id;
+    private String companyName;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
