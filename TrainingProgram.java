eb1210f02af71ee4a8e196782ade3da28ff0dd29
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 16:24:35 -0500

    Create TrainingProgram.java

---
+++ TrainingProgram.java
@@ -0,0 +1,57 @@
+public class TrainingProgram {
+    private String id;
+    private String name;
+    private String description;
+    private List<Certification> certifications;
+    private List<Internship> internships;
+    private List<Employment> employments;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Certification> getCertifications() {
+        return certifications;
+    }
+
+    public void setCertifications(List<Certification> certifications) {
+        this.certifications = certifications;
+    }
+
+    public List<Internship> getInternships() {
+        return internships;
+    }
+
+    public void setInternships(List<Internship> internships) {
+        this.internships = internships;
+    }
+
+    public List<Employment> getEmployments() {
+        return employments;
+    }
+
+    public void setEmployments(List<Employment> employments) {
+        this.employments = employments;
+    }
+}
