12d80f7bb361f5f5d55a2762c801dd4c013a2aea
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 16:29:12 -0500

    Create Employment.java

---
+++ Employment.java
@@ -0,0 +1,39 @@
+public class Employment {
+    private String id;
+    private String companyName;
+    private String position;
+    private Date startDate;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+}
