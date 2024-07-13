f601ba794f7a5f8b64312b76095d14d9cd1215b2
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 16:32:13 -0500

    Create Project.java

---
+++ Project.java
@@ -0,0 +1,22 @@
+public class Project {
+    private String id;
+    private String name;
+    private String description;
+    private double budget;
+    private String partnerName;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name
