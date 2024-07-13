84dce23daa373619b2b57f215fad1b970e573703
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 16:30:41 -0500

    Create TrainingCenter.java

---
+++ TrainingCenter.java
@@ -0,0 +1,39 @@
+public class TrainingCenter {
+    private String id;
+    private String name;
+    private String location;
+    private List<TrainingProgram> trainingPrograms;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<TrainingProgram> getTrainingPrograms() {
+        return trainingPrograms;
+    }
+
+    public void setTrainingPrograms(List<TrainingProgram> trainingPrograms) {
+        this.trainingPrograms = trainingPrograms;
+    }
+}
