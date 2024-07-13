448eaf2fb78aad5447204565be41e43d4816060f
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 16:58:23 -0500

    Create Show.java

---
+++ Show.java
@@ -0,0 +1,33 @@
+public class Show {
+    private String id;
+    private String title;
+    private String description;
+    private Date airDate;
+    private List<Segment> segments;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+   
