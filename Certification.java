2bbf71a4ddbbf54998f8e689dd5533d2b21d858c
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 16:26:09 -0500

    Create Certification.java
    public class Certification {
        private String id;
        private String name;
        private String issuingOrganization;
    
        // Getters and Setters
        public String getId() {
            return id;
        }
    
        public void setId(String id) {
            this.id = id;
        }
    
        public String getName() {
            return name;
        }
    
        public void setName(String name) {
            this.name = name;
        }
    
        public String getIssuingOrganization() {
            return issuingOrganization;
        }
    
        public void setIssuingOrganization(String issuingOrganization) {
            this.issuingOrganization = issuingOrganization;
        }
    }

---
+++ Certification.java
@@ -0,0 +1,30 @@
+public class Certification {
+    private String id;
+    private String name;
+    private String issuingOrganization;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIssuingOrganization() {
+        return issuingOrganization;
+    }
+
+    public void setIssuingOrganization(String issuingOrganization) {
+        this.issuingOrganization = issuingOrganization;
+    }
+}
