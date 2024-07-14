90cc8335707c4692313a0f62a574b37ee56c54a9
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-13 19:31:37 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,103 +1,45 @@
-package com.blerdU.rootworkapp;
+public class MainActivity extends AppCompatActivity {
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import com.google.android.material.navigation.NavigationView;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    private DrawerLayout drawer;
-    private PoolingSystem poolingSystem;
+    private QoLUserProfileViewModel userProfileViewModel;
+    private QoLAssessmentEngine assessmentEngine;
+    private ResourceMatcher resourceMatcher;
+    private QoLProgressTracker progressTracker;
+    private Button btnAssess;
+    private TextView tvRecommendations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // Initialize ViewModel and other components
+        userProfileViewModel = new ViewModelProvider(this).get(QoLUserProfileViewModel.class);
+        assessmentEngine = new QoLAssessmentEngine();
+        resourceMatcher = new ResourceMatcher();
+        progressTracker = new QoLProgressTracker();
 
-        drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // Setup UI components
+        btnAssess = findViewById(R.id.btnAssess);
+        tvRecommendations = findViewById(R.id.tvRecommendations);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
-        }
-
-        // Initialize PoolingSystem
-        poolingSystem = new PoolingSystem();
-        poolingSystem.addMember(new Member("user1", 5000, 3, MemberType.PROJECT));
-        poolingSystem.addMember(new Member("user2", 4500, 2, MemberType.MENTOR));
-        poolingSystem.addMember(new Member("user3", 6000, 4, MemberType.FINANCE));
-        poolingSystem.addMember(new Member("user4", 5500, 5, MemberType.OPERATIONS));
-        poolingSystem.addMember(new Member("user5", 4800, 1, MemberType.DIGITAL));
+        // Set up button click listener
+        btnAssess.setOnClickListener(v -> handleAssessButtonClick());
     }
 
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    private void handleAssessButtonClick() {
+        QoLUserProfile userProfile = userProfileViewModel.getUserProfile();
+        QoLAssessmentResult assessmentResult = assessmentEngine.assessUser(userProfile);
+        List<Suggestion> suggestions = assessmentResult.getSuggestions();
+
+        // Display recommendations
+        displayRecommendations(suggestions);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment selectedFragment = null;
-
-        switch (item.getItemId()) {
-            case R.id.nav_home:
-                selectedFragment = new HomeFragment();
-                break;
-            case R.id.nav_entrepreneurship:
-                selectedFragment = new EntrepreneurshipFragment();
-                break;
-            case R.id.nav_quality_of_life:
-                selectedFragment = new QualityOfLifeFragment();
-                break;
-            case R.id.nav_artistic_works:
-                selectedFragment = new ArtisticWorksFragment();
-                break;
-            case R.id.nav_environmental_awareness:
-                selectedFragment = new EnvironmentalAwarenessFragment();
-                break;
-            case R.id.nav_health_wellness:
-                selectedFragment = new HealthWellnessFragment();
-                break;
-            case R.id.nav_social_justice:
-                selectedFragment = new SocialJusticeFragment();
-                break;
-            case R.id.nav_pooling:
-                selectedFragment = new PoolingFragment();
-                break;
+    private void displayRecommendations(List<Suggestion> suggestions) {
+        StringBuilder recommendationText = new StringBuilder();
+        for (Suggestion suggestion : suggestions) {
+            recommendationText.append(suggestion.getSuggestionText()).append("\n");
         }
-
-        if (selectedFragment != null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    selectedFragment).commit();
-        }
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        tvRecommendations.setText(recommendationText.toString());
     }
-
-    public PoolingSystem getPoolingSystem() {
-        return poolingSystem;
-    }
-}
+            }
