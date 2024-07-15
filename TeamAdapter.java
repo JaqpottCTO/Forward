b99c6ae3c917f3f26f4639dbcfd47944b65f4976
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-15 10:09:00 -0500

    Create TeamAdapter.java

---
+++ TeamAdapter.java
@@ -0,0 +1,34 @@
+public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder> {
+    private List<Team> teams;
+
+    public TeamAdapter(List<Team> teams) {
+        this.teams = teams;
+    }
+
+    @NonNull
+    @Override
+    public TeamViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_team, parent, false);
+        return new TeamViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TeamViewHolder holder, int position) {
+        Team team = teams.get(position);
+        holder.teamName.setText(team.getTeamName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return teams.size();
+    }
+
+    public class TeamViewHolder extends RecyclerView.ViewHolder {
+        TextView teamName;
+
+        public TeamViewHolder(@NonNull View itemView) {
+            super(itemView);
+            teamName = itemView.findViewById(R.id.teamName);
+        }
+    }
+}
