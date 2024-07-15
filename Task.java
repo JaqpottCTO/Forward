38e5da309f200d7fa4099fa74a69d4df1255e16a
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-15 10:09:47 -0500

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,38 @@
+public class Task {
+    private String taskId;
+    private String taskName;
+    private String taskDescription;
+
+    // Constructors, getters, and setters
+    public Task() {}
+
+    public Task(String taskId, String taskName, String taskDescription) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+}
