b1ec236fdbcb3ee0ef99b6f50b42fee6b267a2fb
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-08-05 09:40:32 -0500

    Create UserProgress.java

---
+++ UserProgress.java
@@ -0,0 +1,15 @@
+public class UserProgress {
+    private boolean isUnlocked;
+    private Map<String, Boolean> completedObjectives;
+
+    public UserProgress() {
+        // Default constructor
+    }
+
+    public UserProgress(boolean isUnlocked, Map<String, Boolean> completedObjectives) {
+        this.isUnlocked = isUnlocked;
+        this.completedObjectives = completedObjectives;
+    }
+
+    // Getters and setters
+}
