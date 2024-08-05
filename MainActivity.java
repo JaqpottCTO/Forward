3d368e0bc546eacd4c23c2d9ea767e8e42dfe9fc
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-08-05 09:51:10 -0500

    Update MainActivity.java
    Added Door functionality.

--- MainActivity.java
+++ MainActivity.java
@@ -1,3 +1,13 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private QoLUserProfileViewModel userProfileViewModel;
@@ -5,9 +15,16 @@ public class MainActivity extends AppCompatActivity {
     private ResourceMatcher resourceMatcher;
     private QoLProgressTracker progressTracker;
     private ShowManager showManager;
+    private DoorObjectiveManager doorObjectiveManager;
+    private ExpertFeedbackManager expertFeedbackManager;
+    private FeedbackManager feedbackManager;
+    private DoorProgressTracker doorProgressTracker;
+
     private Button btnAssess;
     private TextView tvRecommendations;
     private Button btnShowVoting;
+    private Button btnViewDoors;
+    private Button btnFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +37,23 @@ public class MainActivity extends AppCompatActivity {
         resourceMatcher = new ResourceMatcher();
         progressTracker = new QoLProgressTracker();
         showManager = new ShowManager();
+        doorObjectiveManager = new DoorObjectiveManager();
+        expertFeedbackManager = new ExpertFeedbackManager();
+        feedbackManager = new FeedbackManager();
+        doorProgressTracker = new DoorProgressTracker();
 
         // Setup UI components
         btnAssess = findViewById(R.id.btnAssess);
         tvRecommendations = findViewById(R.id.tvRecommendations);
         btnShowVoting = findViewById(R.id.btnShowVoting);
+        btnViewDoors = findViewById(R.id.btnViewDoors);
+        btnFeedback = findViewById(R.id.btnFeedback);
 
         // Set up button click listeners
         btnAssess.setOnClickListener(v -> handleAssessButtonClick());
         btnShowVoting.setOnClickListener(v -> handleShowVotingClick());
+        btnViewDoors.setOnClickListener(v -> handleViewDoorsClick());
+        btnFeedback.setOnClickListener(v -> handleFeedbackClick());
     }
 
     private void handleAssessButtonClick() {
@@ -42,6 +67,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void handleShowVotingClick() {
         Intent intent = new Intent(this, ShowActivity.class);
+        startActivity(intent);
+    }
+
+    private void handleViewDoorsClick() {
+        Intent intent = new Intent(this, DoorActivity.class);
+        startActivity(intent);
+    }
+
+    private void handleFeedbackClick() {
+        Intent intent = new Intent(this, FeedbackActivity.class);
         startActivity(intent);
     }
 
