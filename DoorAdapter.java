686a26953ba01f9092bf9472bc305cfbbad32d91
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-08-25 09:32:37 -0500

    Update DoorAdapter.java

--- DoorAdapter.java
+++ DoorAdapter.java
@@ -16,15 +16,54 @@ public class DoorAdapter extends RecyclerView.Adapter<DoorAdapter.ViewHolder> {
 
     private List<Door> doorList;
 
+    // Constructor
     public DoorAdapter(List<Door> doorList) {
         this.doorList = doorList;
     }
 
+    // Create new views (invoked by the layout manager)
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the item_door layout
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_door, parent, false);
         return new ViewHolder(view);
     }
 
-    @
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the current Door object
+        Door door = doorList.get(position);
+
+        // Bind the Door object data to the view elements
+        holder.doorTitle.setText(door.getTitle());
+        holder.doorDescription.setText(door.getDescription());
+
+        // You can add more functionality here, like setting up click listeners
+        holder.itemView.setOnClickListener(v -> {
+            // Handle click events here
+            // For example, you can start a new activity or show a Toast message
+            // Toast.makeText(v.getContext(), "Clicked on " + door.getTitle(), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return doorList.size();
+    }
+
+    // Provide a reference to the type of views that you are using (custom ViewHolder)
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView doorTitle;
+        public TextView doorDescription;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            // Define the view elements
+            doorTitle = itemView.findViewById(R.id.door_title);
+            doorDescription = itemView.findViewById(R.id.door_description);
+        }
+    }
+}
